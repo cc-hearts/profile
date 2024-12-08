ac29b7781839810a963fe60b632f089ca404dfc9
Carl Chen <71313168+cc-hearts@users.noreply.github.com>
2024-12-08 23:13:15 +0800

    chore: remove eslint disabled rule

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,27 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = {
-    "typescriptreact",
-    "javascriptreact",
-    "typescript",
-    "javascript",
-    "json",
-    "jsonc",
-    "sass",
-    "scss",
-    "markdown",
-  },
-  callback = function()
-    local eslint_config_file = "eslint.config.js"
-    local rood_dir = vim.fn.getcwd()
-    if vim.loop.fs_stat(rood_dir .. "/" .. eslint_config_file) then
-      vim.b.autoformat = false
-    end
-  end,
-})
-
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = "*",
   callback = function()
