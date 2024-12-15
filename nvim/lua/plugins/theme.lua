72a14337e12e91558c4ad2aae5dfd55a9ac3013a
Carl Chen <71313168+cc-hearts@users.noreply.github.com>
2024-12-15 17:09:35 +0800

    Update theme.lua

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,19 +1,4 @@
 return {
-  { "rose-pine/neovim",
-    name = "rose-pine",
-    opts = {
-      disable_italics = true
-    }
-  },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "rose-pine",
-    },
-  },
-
   {
     "folke/trouble.nvim",
     -- opts will be merged with the parent spec
