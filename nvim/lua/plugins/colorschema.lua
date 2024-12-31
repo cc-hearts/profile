38a6d14315193ab6915b2a87220580e964c2bff3
carl-chen <ccheartszzz@gmail.com>
2024-12-31 18:32:39 +0800

    chore: update nvim config

---
+++ nvim/lua/plugins/colorschema.lua
@@ -0,0 +1,30 @@
+return {
+  -- add gruvbox
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+      integrations = {
+        cmp = true,
+        treesitter = true,
+        gitsigns = true,
+        telescope = true,
+        nvimtree = true,
+        notify = true,
+      },
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
