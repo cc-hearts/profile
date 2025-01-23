9b05b7a3f842d509768c719412464ca922b01787
carl-chen <ccheartszzz@gmail.com>
2025-01-23 18:39:28 +0800

    chore: upgrade init

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,23 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local function open_terminal_at_root(is_vertical)
+  local position
+  if is_vertical then
+    position = "right"
+  else
+    position = "bottom"
+  end
+  Snacks.terminal.open(nil, { win = { position = position, width = 30 } })
+end
+
+vim.schedule(function()
+  vim.cmd("Neotree")
+  open_terminal_at_root()
+
+  vim.cmd("resize 15")
+  vim.cmd("stopinsert")
+
+  vim.cmd("wincmd w") -- 切换到另一个窗口
+  vim.cmd("wincmd l") -- 然后调整窗口
+end)
