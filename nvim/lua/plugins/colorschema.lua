91149e66887e4d88d3fb19d7ecbe681bc2f6262b
Carl Chen <ccheartszzz@gmail.com>
2025-06-14 00:05:15 +0800

    fix: fix colorscheme

--- nvim/lua/plugins/colorschema.lua
+++ nvim/lua/plugins/colorschema.lua
@@ -1,32 +1,66 @@
 return {
   {
-    "saghen/blink.cmp",
-    event = { "BufNewFile", "BufReadPost" },
-    version = "1.*",
+    "catppuccin/nvim",
+    name = "catppuccin",
     opts = {
-      keymap = {
-        ["<C-n>"] = {
-          "show",
-          "select_next",
-        },
+      transparent_background = true,
+      no_italic = true,
+      integrations = {
+        cmp = true,
+        treesitter = true,
+        gitsigns = true,
+        telescope = true,
+        nvimtree = true,
+        notify = true,
       },
-      completion = {
-        documentation = { auto_show = true },
-        accept = {
-          auto_brackets = {
-            enabled = false,
-          },
-        },
-        menu = {
-          min_width = 24,
-          treesitter = {},
-        },
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+
+  -- -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
       },
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
       },
-      signature = {
-        enabled = true,
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
       },
     },
   },
