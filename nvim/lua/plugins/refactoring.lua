f3621bc59217832dc78224da1f45dfc4c37f2309
Carl Chen <ccheartszzz@gmail.com>
2026-05-25 20:06:18 +0800

    fix(nvim): lazy sync error

--- nvim/lua/plugins/refactoring.lua
+++ nvim/lua/plugins/refactoring.lua
@@ -2,7 +2,28 @@ return {
   {
     "ThePrimeagen/refactoring.nvim",
     dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
       "lewis6991/async.nvim",
     },
+    keys = {
+      {
+        "<leader>rs",
+        function()
+          require("refactoring").select_refactor()
+        end,
+        mode = { "n", "x" },
+        desc = "Refactor",
+      },
+    },
+    config = function(_, opts)
+      require("refactoring").setup(opts)
+
+      if package.searchpath("telescope._extensions.refactoring", package.path) then
+        LazyVim.on_load("telescope.nvim", function()
+          require("telescope").load_extension("refactoring")
+        end)
+      end
+    end,
   },
 }
